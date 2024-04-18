6b221320fb2ab1124a0a5dd369ce89eddf656582
arrrinako <arina.kokhanova@yandex.ru>
2024-04-18 21:04:51 +0300

    Исправила httpUtils.staticFile(res, publicUrl, extname);

--- kb-backend-week-2-startkit-main/controllers/main.js
+++ kb-backend-week-2-startkit-main/controllers/main.js
@@ -6,6 +6,6 @@ async function mainRouteController(res, publicUrl, extname) {
     const data = await getData(endpoints.games);
     await makeRatingFile(config.PATH_TO_RATING_FILE, data);
     res.statusCode = 200;
-    staticFile(res, publicUrl, extname);
+    httpUtils.staticFile(res, publicUrl, extname);
 }
   module.exports = mainRouteController;
